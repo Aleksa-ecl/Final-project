2b6685b0c47c904e6616430d66644f68d3c80851
Aleksandra Moldovan <moldovanaleksandra@yandex.ru>
2025-08-22 01:14:01 +0300

    Final project

---
+++ configuration.py
@@ -0,0 +1,6 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+URL_SERVICE = "https://a4fdfac5-a616-458a-bb3a-6399b82e47b4.serverhub.praktikum-services.ru"
+# Cоздание заказа
+CREATE_ORDER = "/api/v1/orders"
+# Получить заказ по его номеру
+TRACK_ORDER = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,16 @@
+headers = {
+    "Content-Type": "application/json"
+}
+create_order_body = {
+    "firstName": "Aleksa",
+    "lastName": "Mold",
+    "address": "Moscow, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 543 01 00",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Come back to Moscow",
+    "color": [
+        "BLACK"
+    ]
+}
