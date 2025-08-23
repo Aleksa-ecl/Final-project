6a8449af84163fa7dae2f377a792b0e91f7d54c1
Aleksa-ecl <moldovanaleksandra@yandex.ru>
2025-08-24 01:34:06 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -12,9 +12,11 @@ def post_new_order(body):
 response = post_new_order(data.create_order_body)
 track = response.json()["track"]
 
+def create_order(body):
+    #Функция для создания заказа
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
+                        json=body,
+                        headers={"Content-Type":"application/json"})
 
-def get_track_order():
-    return requests.get(configuration.URL_SERVICE + configuration.TRACK_ORDER + "?t=" + str(track))
-
-
-response = get_track_order()
+def get_track_order(track):
+    return requests.get(configuration.URL_SERVICE + configuration.TRACK_ORDER, params={"t": track} )
