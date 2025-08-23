2210910190c3cdc4d1c2c3f593906503e86d932e
Aleksa-ecl <moldovanaleksandra@yandex.ru>
2025-08-24 01:32:44 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
-URL_SERVICE = "https://a4fdfac5-a616-458a-bb3a-6399b82e47b4.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://40b8f202-6e3f-4c0e-a0ab-4ffdd93b463e.serverhub.praktikum-services.ru"
 # Cоздание заказа
 CREATE_ORDER = "/api/v1/orders"
 # Получить заказ по его номеру
