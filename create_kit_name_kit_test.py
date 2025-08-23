1f70cdf384ea25f639fab2ddc99653eaadde673d
Aleksa-ecl <moldovanaleksandra@yandex.ru>
2025-08-24 01:33:23 +0300

    Update create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,6 +1,20 @@
+# Молдован Александра, 33-я когорта — Финальный проект. Инженер по тестированию расширенный 
 import sender_stand_request
 import data
 
+
 def test_track_order():
-    response = sender_stand_request.get_track_order()
-    assert response.status_code == 200
+    # Запрос на создание заказа
+    create_order_response = sender_stand_request.create_order(data.create_order_body)
+    
+    # Проверка, что заказ создался успешно
+    assert create_order_response.status_code == 201
+    
+    # Сохранить номер трека заказа
+    track_number = create_order_response.json()["track"]
+    
+    # Запрос на получение заказа по треку заказа
+    get_order_response = sender_stand_request.get_track_order(track_number)
+    
+    # Проверка, что код ответа равен 200
+    assert get_order_response.status_code == 200
